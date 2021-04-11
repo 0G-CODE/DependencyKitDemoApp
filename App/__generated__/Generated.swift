d0b4aa9a591ad48c559380a29e36c858ac2e035a
Adam Zethraeus <adam@zethrae.us>
2021-04-11 03:10:26 -0700

    add (very incorrectly) generated files

---
+++ App/__generated__/Generated.swift
@@ -0,0 +1,56 @@
+/*
+###################
+# module: DemoApp #
+###################
+# imports: 
+# - Foundation
+# - NetworkClient
+# - DependencyKit
+# - QuartzCore
+# - Combine
+# - SwiftUI
+#-------------------
+# requirements: 
+# - protocol ScopeTwoRequirements: Requirements,
+#     GENERATED_IMPLICIT_ScopeTwoRequirements {
+#       var explicit: String
+#       var modified: String
+#       var recreated: String
+#       var createdLater: String
+#   }
+# - protocol ScopeThreeRequirements: Requirements,
+#     GENERATED_IMPLICIT_ScopeThreeRequirements {
+#       var explicit: String
+#       var implicit: String
+#       var modified: String
+#       var recreated: String
+#       var createdLater: String
+#       var duplicated: String
+#       var optional: String?
+#   }
+# - protocol ScopeOneRequirements: Requirements,
+#     GENERATED_IMPLICIT_ScopeOneRequirements {
+#       var explicit: String
+#       var modified: String
+#   }
+#-------------------
+# resources: 
+# - class ScopeTwoResource<I: ScopeTwoRequirements>: Resource<I>,
+#     ScopeThreeRequirements {
+#       // TODO: indicate implementation
+#   }
+# - class ScopeZeroResource<I: NilRequirements>: Resource<I>,
+#     ScopeOneRequirements {
+#       // TODO: indicate implementation
+#   }
+# - class ScopeThreeResource<I: ScopeThreeRequirements>: Resource<I> {
+#       // TODO: indicate implementation
+#   }
+# - class ScopeOneResource<I: ScopeOneRequirements>: Resource<I>,
+#     NetworkClientRequirements,
+#     ScopeTwoRequirements {
+#       // TODO: indicate implementation
+#   }
+#-------------------
+
+*/
---
+++ Libraries/NetworkClient/NetworkClient/__generated__/Generated.swift
@@ -0,0 +1,21 @@
+/*
+#########################
+# module: NetworkClient #
+#########################
+# imports: 
+# - DependencyKit
+# - Combine
+# - Foundation
+#-------------------------
+# requirements: 
+# - public protocol NetworkClientRequirements: Requirements {
+#       var networkMonitor: NetworkMonitorInterface?
+#   }
+#-------------------------
+# resources: 
+# - public class NetworkClientResource<I: NetworkClientRequirements>: Resource<I> {
+#       // TODO: indicate implementation
+#   }
+#-------------------------
+
+*/
